66e0d35a5bd79680979314bbd2bb9c177d3a05da
songmath-thunderain <yongjis@clemson.edu>
2022-11-09 10:38:24 -0500

    just clean up

--- random/run.py
+++ random/run.py
@@ -8,13 +8,14 @@ command = []
 
 instanceAttr1 = [" 3 ", " 6 ", " 9 "];
 instanceAttr2 = [" 10 ", " 20 ", " 30 "];
-instanceAttr3 = [" 0.005 "];
+instanceAttr3 = [" 0.005 ", " 0.05 ", " 0.5 ", " 5 "];
 
 for k1 in range(len(instanceAttr3)):
     counter = 10*k1;
     for k2 in range(len(instanceAttr1)):
+        for k3 in range(len(instanceAttr2)):
             counter = counter + 1;
-            command = command + ["julia MAIN.jl " + str(counter) + instanceAttr1[k2] + instanceAttr2[k2] + instanceAttr3[k1] + " 1000 65 100 "];			
+            command = command + ["julia MAINCV.jl " + str(counter) + instanceAttr1[k2] + instanceAttr2[k3] + instanceAttr3[k1] + " 1000 65 100 "];			
 
 for i in range(len(command)):
     print(command[i]);
