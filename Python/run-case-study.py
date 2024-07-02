5f876da379697a7838b8e21b936f0c8af789fe1f
songmath-thunderain <yongjis@clemson.edu>
2024-07-02 09:16:52 -0400

    major bug identified in FA

--- Python/run-case-study.py
+++ Python/run-case-study.py
@@ -3,14 +3,25 @@ import time
 
 command = []
 
-instance_option = [" 1 ", " 2 "];
-fc_level = [" 0.25 "]
+instance_option = [" 2 "];
+inv_level = [" 0 ", " 0.5 ", " 1 ", " 2 "]
+fc_level = [" 0.25 ", " 0.5 ", " 1 ", " 2 ", " 4 "]
 st_level = [" 3 ", " 4 "]
 
 for k1 in instance_option:
+    for k2 in inv_level:
+            command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t 1000000 " + "-s 1 -i " + k1 + "-w 1 -c 2 " + "-st 2 " + "-inv " + k2];
+
+for k1 in instance_option:
     for k2 in fc_level:
-        for k3 in st_level:
-            command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t 1000000 " + "-s 6 -i " + k1 + "-w 1 -c 2 " + "-st " + k3 + "-fc " + k2];
+        command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t 1000000 " + "-s 1 -i " + k1 + "-w 1 -c 2 " + "-st 2 " + "-fc " + k2];
+
+
+for k1 in instance_option:
+    for k2 in st_level:
+        command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t 1000000 " + "-s 1 -i " + k1 + "-w 1 -c 2 " + "-st " + k2];
+
+
 
 for i in range(len(command)):
     print(command[i]);
