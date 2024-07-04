e11dd25d7285a98c14b379db6f7efaaba4a1f88b
songmath-thunderain <yongjis@clemson.edu>
2024-07-04 01:10:09 -0400

    results update

--- Python/run-case-study.py
+++ Python/run-case-study.py
@@ -5,12 +5,12 @@ command = []
 
 instance_option = [" 1 ", " 2 "];
 
-cost_factor = [" 1 ", " 2 ", " 5 ", " 10 "];
+st_levels = [" 1 ", " 3 ", " 4 "];
+
 
 for k1 in instance_option:
-    for k3 in cost_factor:
-        command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t " + k3 + "-s 3 -i " + k1 + "-w 1 -c 1 " + "-st 2 " + "-trans 10 "];
-    command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t 1000000 " + "-s 3 -i " + k1 + "-w 1 -c 2 " + "-st 2 " + "-trans 10 "];
+    for k3 in st_levels:
+        command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t 1000000 " + "-s 3 -i " + k1 + "-w 1 -c 2 " + "-st " + k3 + "-trans 10 "];
 
 for i in range(len(command)):
     print(command[i]);
