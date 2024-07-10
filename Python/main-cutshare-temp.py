62889d2e647880e20439b1c2c8e7da65c4dbfd80
songmath-thunderain <yongjis@clemson.edu>
2024-07-10 13:51:11 -0400

    Update main-cutshare-temp.py

--- Python/main-cutshare-temp.py
+++ Python/main-cutshare-temp.py
@@ -79,9 +79,6 @@ if __name__ == "__main__":
         time_limit = params['TIME_LIMIT']
         cutviol = params['CUTVIOL']
 
-    #if args.solution_option == 0:
-    #    stall = stall*10
-
     inputParams = inputParams(dissipate_option, absorbing_option, cost_structure, safe_time, tau, k_init, oos);
     solveParams = solveParams(max_iter, stall, cutviol_maxiter, time_limit, cutviol);
 
