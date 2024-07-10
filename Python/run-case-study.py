c6437c8c2fc8d26e2dfffc97a343d1d3f134a973
songmath-thunderain <yongjis@clemson.edu>
2024-07-10 07:43:44 -0400

    Update run-case-study.py

--- Python/run-case-study.py
+++ Python/run-case-study.py
@@ -3,18 +3,39 @@ import time
 
 command = []
 
-instance_option = [" 1 "];
+instance_option = [" 1 ", " 2 "];
 
-pen_levels = [" 0.25 ", " 0.5 ", " 2 ", " 4 "];
+cost_structure = [" 1 ", " 2 "]
+
+nu_levels = [" 1 ", " 2 ", " 5 ", " 10 "];
 
 fc_levels = [" 0.25 ", " 0.5 ", " 2 ", " 4 "]
 
+st_levels = [" 1 ", " 3 ", " 4 "]
+
+pen_levels = [" 0.25 ", " 0.5 ", " 2 ", " 4 "]
+
+inv_levels = [" 0.25 ", " 0.5 ", " 2 ", " 4 "]
+
 for k1 in instance_option:
-    for k3 in pen_levels:
-        command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t 1000000 " + "-s 3 -i " + k1 + "-w 0 -c 2 " + "-st 2 " + "-pen " + k3 + "-trans 10 "];
+    for k2 in cost_structure:
+        if k2 == " 1 ":
+            for k3 in nu_levels:
+                command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t " + k3 + "-s 1 -i " + k1 + "-w 1 -c 1 " + "-st 2 " + "-trans 10 "];
+        if k2 == " 2 ":
+            command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t 1000000 " + "-s 1 -i " + k1 + "-w 1 -c 2 " + "-st 2 " + "-trans 10 "];
 
     for k3 in fc_levels:
-        command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t 2 " + "-s 3 -i " + k1 + "-w 0 -c 1 " + "-st 2 " + "-fc " + k3 + "-trans 10 "];
+        command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t 1000000 " + "-s 1 -i " + k1 + "-w 1 -c 2 " + "-st 2 " + "-fc " + k3 + "-trans 10 "];
+    
+    for k3 in pen_levels:
+        command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t 1000000 " + "-s 1 -i " + k1 + "-w 1 -c 2 " + "-st 2 " + "-pen " + k3 + "-trans 10 "];
+    
+    for k3 in inv_levels:
+        command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t 1000000 " + "-s 1 -i " + k1 + "-w 1 -c 2 " + "-st 2 " + "-inv " + k3 + "-trans 10 "];
+
+    for k3 in st_levels:
+        command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t 1000000 " + "-s 1 -i " + k1 + "-w 1 -c 2 " + "-st " + k3 + "-trans 10 "];
 
 for i in range(len(command)):
     print(command[i]);
