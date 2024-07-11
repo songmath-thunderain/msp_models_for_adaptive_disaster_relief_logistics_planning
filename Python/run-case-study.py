1fe64c97498666439a65194e62c99d03077a327f
songmath-thunderain <yongjis@clemson.edu>
2024-07-11 00:30:02 -0400

    A bunch of results for the case study are updated

--- Python/run-case-study.py
+++ Python/run-case-study.py
@@ -15,7 +15,9 @@ st_levels = [" 1 ", " 3 ", " 4 "]
 
 pen_levels = [" 0.25 ", " 0.5 ", " 2 ", " 4 "]
 
-inv_levels = [" 0.25 ", " 0.5 ", " 2 ", " 4 "]
+inv_levels = [" 0 ", " 0.5 ", " 2 "]
+
+trans_levels = [" 2.5 ", " 5 ", " 20 ", " 40 "]
 
 for k1 in instance_option:
     for k2 in cost_structure:
@@ -36,6 +38,9 @@ for k1 in instance_option:
 
     for k3 in st_levels:
         command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t 1000000 " + "-s 1 -i " + k1 + "-w 1 -c 2 " + "-st " + k3 + "-trans 10 "];
+
+    for k3 in trans_levels:
+        command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t 1000000 " + "-s 1 -i " + k1 + "-w 1 -c 2 " + "-st 2 " + "-trans " + k3];
 
 for i in range(len(command)):
     print(command[i]);
