42ddfeca1525bdd3fafbc0be0285c2b0e98b66d6
songmath-thunderain <yongjis@clemson.edu>
2024-07-22 11:02:18 -0400

    minor updates

--- Python/run-case-study.py
+++ Python/run-case-study.py
@@ -17,15 +17,15 @@ for k1 in instance_option:
     for k2 in cost_structure:
         if k2 == " 1 ":
             for k3 in nu_levels:
-                command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t " + k3 + "-s 2 -i " + k1 + "-w 1 -c 1 " + "-st 2 " + "-trans 10 "];
+                command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t " + k3 + "-s 3 -i " + k1 + "-w 1 -c 1 " + "-st 2 " + "-trans 10 "];
         if k2 == " 2 ":
-            command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t 1000000 " + "-s 2 -i " + k1 + "-w 1 -c 2 " + "-st 2 " + "-trans 10 "];
+            command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t 1000000 " + "-s 3 -i " + k1 + "-w 1 -c 2 " + "-st 2 " + "-trans 10 "];
 
     for k3 in fc_levels:
-        command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t 1000000 " + "-s 2 -i " + k1 + "-w 1 -c 2 " + "-st 2 " + "-fc " + k3 + "-trans 10 "];
+        command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t 1000000 " + "-s 3 -i " + k1 + "-w 1 -c 2 " + "-st 2 " + "-fc " + k3 + "-trans 10 "];
     
     for k3 in st_levels:
-        command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t 1000000 " + "-s 2 -i " + k1 + "-w 1 -c 2 " + "-st " + k3 + "-trans 10 "];
+        command = command + ["python main.py -p solveParams.yaml -d 0 -a 0 " + "-k 1 -o 1000 -ni 3 -nj 10 -t 1000000 " + "-s 3 -i " + k1 + "-w 1 -c 2 " + "-st " + k3 + "-trans 10 "];
 
 for i in range(len(command)):
     print(command[i]);
